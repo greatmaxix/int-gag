fb2d24b0c87febd46be4625b366c52d6ec83ecc7
Waddle-hub <Wadefriedrichs42@gmail.com>
2020-03-05 11:06:02 +0100

    Added pieces of controller and added route

---
+++ laravel/app/Http/Controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php 
+     namespace App\Http\Controllers;
+
+     class MainController extends Controller
+     {
+
+         function index(){
+            return view('INT-GAG.Mainpage');
+         }
+
+     }
+     
+?>
