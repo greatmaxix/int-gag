58d8f90f71b80f107da842ec5b18ea14fae69032
Waddle-hub <Wadefriedrichs42@gmail.com>
2020-03-26 11:24:24 +0100

    Minor bug fix with route name

--- laravel/resources/views/INT-GAG/Mainpage.blade.php
+++ laravel/resources/views/INT-GAG/Mainpage.blade.php
@@ -20,7 +20,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="{{route("Mainpage")}}">Home<span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="{{route("INT-GAG")}}">Home<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Premium</a>
