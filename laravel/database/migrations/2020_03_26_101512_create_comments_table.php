65edfa3b8c3c2bf350624531d58a7095b6d41872
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-03-26 11:17:01 +0100

    Migrations

--- laravel/database/migrations/2020_03_26_101512_create_comments_table.php
+++ laravel/database/migrations/2020_03_26_101512_create_comments_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class CreateCommentsTable extends Migration
+class CreateVotesCountTable extends Migration
 {
     /**
      * Run the migrations.
@@ -35,6 +35,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('votes_count');
     }
 }
