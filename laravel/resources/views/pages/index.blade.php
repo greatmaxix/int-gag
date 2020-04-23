9b14cb884e75707058266ed9ed8d519c80c31754
56112755 <Wadefriedrichs42@gmail.com>
2020-04-23 10:40:49 +0200

    made all of the pages, profile, posts, and services

---
+++ laravel/resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+    <h1>{{$title}}</h1>
+    <p>I am making a laravel applciation </p>
+    <p>
+    </p>
+</div>
+@endsection
---
+++ laravel/resources/views/posts/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Create Posts !</h1>
+
+    <form method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+        <div class="form-group">
+            @csrf            
+            <label for="title">Title</label>
+            <input type="text" class="form-control" name="title" placeholder="Title"/>
+        </div>
+        <div class="form-group">
+            <label for="Body">Body</label>
+            <textarea class="form-control" name="Body" cols="30" rows="10" placeholder="Body Text"></textarea>
+        </div>
+        <div class="form-group">
+            <input type="file" name="cover_image" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Create Post</button>
+    </form>
+
+@endsection
