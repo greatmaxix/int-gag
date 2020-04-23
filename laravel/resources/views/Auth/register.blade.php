31ebbcab66c331737f3d2d912614d2c5b69073b8
Xolani Nhlapo <Nhlapho7@gmail.com>
2020-04-23 10:38:17 +0200

    Auth: Login fix

--- laravel/resources/views/Auth/register.blade.php
+++ laravel/resources/views/Auth/register.blade.php
@@ -1,75 +1,77 @@
-@extends('INT-GAG.Mainpage')
+@extends('layouts.app')
 
 @section('content')
-    
-<div class="w-50 mx-auto">
-    <form action="{{ route('register') }}" method="POST">
-        @csrf 
-        
-        <div class="row">
-            <div class="col">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
 
-        <div class="row">
-            <div class="col">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-                <div class="form-group">
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
-                    <label for="password">Password</label>
-                    <input id="password" class="form-control {{$errors->has('password') ? 'is-invalid' : ' '}}" type="text" name="password">
-                    @if ($errors->has('password'))
-                      @foreach ($errors->get('password') as $error)
-                        <p class="invalid-feedback">{{$error}}</p>
-                      @endforeach
-                    @endif
-          
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-            </div>
-            <div class="col">
-
-                <div class="form-group">
-
-                    <label for="password_confirmation">Confirm Password</label>
-                    <input id="password_confirmation" class="form-control {{$errors->has('password_confirmation') ? 'is-invalid' : ' '}}" type="text" name="password_confirmation">
-                    @if ($errors->has('password_confirmation'))
-                      @foreach ($errors->get('password_confirmation') as $error)
-                        <p class="invalid-feedback">{{$error}}</p>
-                      @endforeach
-                    @endif
-          
-                </div>
-
             </div>
         </div>
-
-        <div class="form-group">
-
-            <label for="username">User name</label>
-            <input id="username" class="form-control {{$errors->has('username') ? 'is-invalid' : ' '}}" type="text" name="username">
-            @if ($errors->has('username'))
-              @foreach ($errors->get('username') as $error)
-                <p class="invalid-feedback">{{$error}}</p>
-              @endforeach
-            @endif
-
-        </div>
-
-        <div class="form-group">
-
-            <label for="email">Email</label>
-            <input id="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ' '}}" type="text" name="email">
-            @if ($errors->has('email'))
-              @foreach ($errors->get('email') as $error)
-                <p class="invalid-feedback">{{$error}}</p>
-              @endforeach
-            @endif
-
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-outline-light" type="submit">Register</button>
-        </div>
-    </form>
+    </div>
 </div>
-
 @endsection
