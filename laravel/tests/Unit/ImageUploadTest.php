e5b130b878a6c8812a66d5b5214113aff31dd081
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-04-30 10:27:05 +0200

    New Post Test

---
+++ laravel/tests/Unit/ImageUploadTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ImageUploadTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
