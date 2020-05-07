6146c48e7da9a21c1b925aec5ad51a69b04b08b1
KaddourSrarfi <kaddoursrarfi@yahoo.fr>
2020-05-07 11:08:16 +0200

    French language / logo pic

---
+++ laravel/resources/lang/fr/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language lines 
+    | You can choose a specific language here 
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    | Possibilities of laguages available : English / French 
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
