4f827aa391f7264ea04661301a3f2559108e4bb3
Kaddour Srarfi <kaddoursrarfi@yahoo.fr>
2020-05-07 11:23:01 +0200

    Update pagination.php

--- laravel/resources/lang/fr/pagination.php
+++ laravel/resources/lang/fr/pagination.php
@@ -4,13 +4,11 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Pagination Language Lines
+    | Les langues de pages
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used by the paginator library to build
-    | the simple pagination links. You are free to change them to anything
-    | you want to customize your views to better match your application.
-    |
+    | vous pouvez changer votre langue pour les pages dependant de vos choix et votre application pour mieux explorer 
+    |  le site web
     */
 
     'previous' => '&laquo; Previous',
