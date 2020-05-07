5e189df39e32d20f70c06d71e43c6c863e1966a3
Kaddour Srarfi <kaddoursrarfi@yahoo.fr>
2020-05-07 11:26:22 +0200

    Update validation.php

--- laravel/resources/lang/fr/validation.php
+++ laravel/resources/lang/fr/validation.php
@@ -4,12 +4,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Validation Language Lines
+    | La Validation
     |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
+    | ces lignes sont pour les messages d'erreur utiliser par la classe Validation
+    | 
     |
     */
 
